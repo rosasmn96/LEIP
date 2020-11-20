36fb639a96d00bfed8d1c262cc2d55d033414f0a
mrosas96 <madison.rosas96@gmail.com>
2020-11-20 12:07:29 -0500

    Added: ISP Python and HTML files Edited: Home Python and HTML files

--- app/home.py
+++ app/home.py
@@ -6,5 +6,5 @@ app = Flask(__name__)
 def home():
     return render_template('home.html')
 
-if __name__ == "__main__":
+if __name__ == '__main__':
     app.run()
--- app/isp.py
+++ app/isp.py
@@ -1,10 +1,29 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
+import cx_Oracle
+import os
+import pandas
+
+ispName = ""
 
 app = Flask(__name__)
 
-@app.route('/')
-def isp():
-    return render_template('isp.html')
+def ispdb():
+    os.environ['PATH'] = '/Oracle/instantclient_19_8'
+    os.environ['TNS_ADMIN'] = '/Users/madis/Documents/ITNS&SD Sem 10/Senior Design 1/Project/LEIP Database/Wallet'
+    con = cx_Oracle.connect('ADMIN', 'L31P_P@$$-w0rd!', 'leip_high')
+    cursor = con.cursor()
+    cursor.execute("SELECT * FROM ISP WHERE NAME = 'Apple Inc'")
+    ispdata = pandas.DataFrame(cursor.fetchall())
+    print(ispdata)
 
-if __name__ == "__main__":
-    app.run()
+ispdb()
+
+#@app.route('/isp')
+#def isp():
+    #if request.method == "POST":
+        #ispName = request.form['ispName']
+        
+   # return render_template('isp.html')
+
+#if __name__ == "__main__":
+    #app.run()
