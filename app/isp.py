3005a697b7efe06181c11f886dc582528d099580
mrosas96 <madison.rosas96@gmail.com>
2020-11-22 10:27:04 -0500

    Created ispResult.html; Got working code in original isp files - allows search results to be posted on isp result page

--- app/isp.py
+++ app/isp.py
@@ -1,29 +1,24 @@
 from flask import Flask, render_template, request
 import cx_Oracle
 import os
-import pandas
-
-ispName = ""
 
 app = Flask(__name__)
 
-def ispdb():
+ispName = ""
+
+@app.route('/isp', methods=['GET'])
+def isp():
+    return render_template('isp.html')
+
+@app.route('/ispResult', methods=['POST'])
+def ispPost():
+    ispName = request.form['ispName']
     os.environ['PATH'] = '/Oracle/instantclient_19_8'
     os.environ['TNS_ADMIN'] = '/Users/madis/Documents/ITNS&SD Sem 10/Senior Design 1/Project/LEIP Database/Wallet'
     con = cx_Oracle.connect('ADMIN', 'L31P_P@$$-w0rd!', 'leip_high')
     cursor = con.cursor()
-    cursor.execute("SELECT * FROM ISP WHERE NAME = 'Apple Inc'")
-    ispdata = pandas.DataFrame(cursor.fetchall())
-    print(ispdata)
+    ispRow = cursor.execute("SELECT * FROM ISP WHERE NAME = '" + ispName + "'").fetchone()
+    return render_template('ispResult.html', ispRow=ispRow)    
 
-ispdb()
-
-#@app.route('/isp')
-#def isp():
-    #if request.method == "POST":
-        #ispName = request.form['ispName']
-        
-   # return render_template('isp.html')
-
-#if __name__ == "__main__":
-    #app.run()
+if __name__ == "__main__":
+    app.run()
