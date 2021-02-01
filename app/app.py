bed44c8da5394854719838d68206d7460c680b21
mrosas96 <madison.rosas96@gmail.com>
2021-02-01 18:03:57 -0500

    Got the phone directory page to a working state. Updated code for phone directory in app.py and PhoneDirectory.html.

--- app/app.py
+++ app/app.py
@@ -1,6 +1,7 @@
 from flask import Flask, render_template, request
 import cx_Oracle
 import os
+import array as arr
 
 app = Flask(__name__, static_folder='C:\\LEIP\\app\\assets')
 
@@ -18,12 +19,12 @@ def phoneDirectory():
     os.environ['TNS_ADMIN'] = '/Users/madis/Documents/ITNS&SD Sem 10/Senior Design 1/Project/LEIP Database/Wallet'
     con = cx_Oracle.connect('ADMIN', 'L31P_P@$$-w0rd!', 'leip_high')
     cursor = con.cursor()
-    contactAgency = cursor.execute("SELECT AGENCY FROM PHONEDIRECTORY").fetchone()
-    contactCounty = cursor.execute("SELECT COUNTY FROM PHONEDIRECTORY").fetchone()
-    contactTownship = cursor.execute("SELECT TOWNSHIP FROM PHONEDIRECTORY").fetchone()
-    contactCincyDirectory = cursor.execute("SELECT CINCINNATI_PHONE_DIRECTORY FROM PHONEDIRECTORY").fetchone()
-    contactNumber = cursor.execute("SELECT PHONE_NUMBER FROM PHONEDIRECTORY").fetchone()
-    return render_template('PhoneDirectory.html', contactAgency=contactAgency, contactCounty=contactCounty, contactTownship=contactTownship, contactCincyDirectory=contactCincyDirectory, contactNumber=contactNumber)
+    contactAgencies = (cursor.execute("SELECT AGENCY FROM PHONEDIRECTORY").fetchall())
+    contactCounties = (cursor.execute("SELECT COUNTY FROM PHONEDIRECTORY").fetchall())
+    contactTownships = (cursor.execute("SELECT TOWNSHIP FROM PHONEDIRECTORY").fetchall())
+    contactCincyDirectories = (cursor.execute("SELECT CINCINNATI_PHONE_DIRECTORY FROM PHONEDIRECTORY").fetchall())
+    contactNumbers = (cursor.execute("SELECT PHONE_NUMBER FROM PHONEDIRECTORY").fetchall())
+    return render_template('PhoneDirectory.html', contactAgencies=contactAgencies, contactCounties=contactCounties, contactTownships=contactTownships, contactCincyDirectories=contactCincyDirectories, contactNumbers=contactNumbers)
 
 @app.route('/preservedigitalevidence', methods=['GET'])
 def digitalEvidence():
