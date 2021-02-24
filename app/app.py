d695c32e90f6af28330e6d96ae4213e5d84576fc
mrosas96 <madison.rosas96@gmail.com>
2021-02-24 15:00:25 -0500

    ISP search results now showing on same page as dropdown input

--- app/app.py
+++ app/app.py
@@ -31,33 +31,68 @@ def phoneDirectory():
 def digitalEvidence():
     return render_template('Evidence.html')
 
-@app.route('/isp', methods=['GET'])
+@app.route('/isp', methods=['GET', 'POST'])
 def isp():
+    httpMethod = 'GET'
     os.environ['PATH'] = '/Oracle/instantclient_19_8'
     os.environ['TNS_ADMIN'] = '/Users/madis/Documents/ITNS&SD Sem 10/Senior Design 1/Project/LEIP Database/Wallet'
     con = cx_Oracle.connect('ADMIN', 'L31P_P@$$-w0rd!', 'leip_high')
     cursor = con.cursor()
-    ispName = cursor.execute("SELECT name FROM isp ORDER BY name").fetchall()
-    return render_template('ISP.html', ispName=ispName)
-
-@app.route('/ispresult', methods=['POST'])
-def ispPost():
-    ispIndex = int(request.form['ispIndex'])
-    os.environ['PATH'] = '/Oracle/instantclient_19_8'
-    os.environ['TNS_ADMIN'] = '/Users/madis/Documents/ITNS&SD Sem 10/Senior Design 1/Project/LEIP Database/Wallet'
-    con = cx_Oracle.connect('ADMIN', 'L31P_P@$$-w0rd!', 'leip_high')
-    cursor = con.cursor()
-    ispNameColumn = cursor.execute("SELECT name FROM isp ORDER BY name").fetchall()
-    ispName = ''.join(ispNameColumn[ispIndex])
-    ispContact = ''.join(cursor.execute("SELECT contact_or_attn FROM isp WHERE name='" + ispName + "'").fetchone())
-    ispLocation = ''.join(cursor.execute("SELECT location FROM isp WHERE name='" + ispName + "'").fetchone())
-    ispFaxNumber = ''.join(cursor.execute("SELECT fax_number FROM isp WHERE name='" + ispName + "'").fetchone())
-    ispEmail = ''.join(cursor.execute("SELECT email FROM isp WHERE name='" + ispName + "'").fetchone())
-    ispInfo = ''.join(cursor.execute("SELECT info FROM isp WHERE name='" + ispName + "'").fetchone())
-    ispLastUpdated = ''.join(cursor.execute("SELECT last_updated FROM isp WHERE name='" + ispName + "'").fetchone())
-    ispLEGuide = ''.join(cursor.execute("SELECT le_guide FROM isp WHERE name='" + ispName + "'").fetchone())
-    ispTimeZone = ''.join(cursor.execute("SELECT time_zone FROM isp WHERE name='" + ispName + "'").fetchone())
-    return render_template('ISPResults.html', ispName=ispName, ispContact=ispContact, ispLocation=ispLocation, ispFaxNumber=ispFaxNumber, ispEmail=ispEmail, ispInfo=ispInfo, ispLastUpdated=ispLastUpdated, ispLEGuide=ispLEGuide, ispTimeZone=ispTimeZone)
+    ispNames = cursor.execute("SELECT name FROM isp ORDER BY name").fetchall()
+    ispName = 'Apple Inc'
+    ispContact = cursor.execute("SELECT contact_or_attn FROM isp WHERE name='" + ispName + "'").fetchone()
+    ispLocation = cursor.execute("SELECT location FROM isp WHERE name='" + ispName + "'").fetchone()
+    ispFaxNumber = cursor.execute("SELECT fax_number FROM isp WHERE name='" + ispName + "'").fetchone()
+    ispEmail = cursor.execute("SELECT email FROM isp WHERE name='" + ispName + "'").fetchone()
+    ispInfo = cursor.execute("SELECT info FROM isp WHERE name='" + ispName + "'").fetchone()
+    ispLastUpdated = cursor.execute("SELECT last_updated FROM isp WHERE name='" + ispName + "'").fetchone()
+    ispLEGuide = cursor.execute("SELECT le_guide FROM isp WHERE name='" + ispName + "'").fetchone()
+    ispTimeZone = cursor.execute("SELECT time_zone FROM isp WHERE name='" + ispName + "'").fetchone()
+    if request.method == 'POST':
+        httpMethod = 'POST'
+        ispIndex = int(request.form['ispIndex'])
+        ispName = ''.join(ispNames[ispIndex])
+        ispContact = cursor.execute("SELECT contact_or_attn FROM isp WHERE name='" + ispName + "'").fetchone()
+        if ispContact == (None,):
+            ispContact = 'N/A'
+        else:
+            ispContact = ''.join(ispContact)
+        ispLocation = cursor.execute("SELECT location FROM isp WHERE name='" + ispName + "'").fetchone()
+        if ispLocation == (None,):
+            ispLocation = 'N/A'
+        else:
+            ispLocation = ''.join(ispLocation)
+        ispFaxNumber = cursor.execute("SELECT fax_number FROM isp WHERE name='" + ispName + "'").fetchone()
+        if ispFaxNumber == (None,):
+            ispFaxNumber = 'N/A'
+        else:
+            ispFaxNumber = ''.join(ispFaxNumber)
+        ispEmail = cursor.execute("SELECT email FROM isp WHERE name='" + ispName + "'").fetchone()
+        if ispEmail == (None,):
+            ispEmail = 'N/A'
+        else:
+            ispEmail = ''.join(ispEmail)
+        ispInfo = cursor.execute("SELECT info FROM isp WHERE name='" + ispName + "'").fetchone()
+        if ispInfo == (None,):
+            ispInfo = 'N/A'
+        else:
+            ispInfo = ''.join(ispInfo)
+        ispLastUpdated = cursor.execute("SELECT last_updated FROM isp WHERE name='" + ispName + "'").fetchone()
+        if ispLastUpdated == (None,):
+            ispLastUpdated = 'N/A'
+        else:
+            ispLastUpdated = ''.join(ispLastUpdated)
+        ispLEGuide = cursor.execute("SELECT le_guide FROM isp WHERE name='" + ispName + "'").fetchone()
+        if ispLEGuide == (None,):
+            ispLEGuide = 'N/A'
+        else:
+            ispLEGuide = ''.join(ispLEGuide)
+        ispTimeZone = cursor.execute("SELECT time_zone FROM isp WHERE name='" + ispName + "'").fetchone()
+        if ispTimeZone == (None,):
+            ispTimeZone = 'N/A'
+        else:
+            ispTimeZone = ''.join(ispTimeZone)
+    return render_template('ISP.html', httpMethod=httpMethod, ispNames=ispNames, ispName=ispName, ispContact=ispContact, ispLocation=ispLocation, ispFaxNumber=ispFaxNumber, ispEmail=ispEmail, ispInfo=ispInfo, ispLastUpdated=ispLastUpdated, ispLEGuide=ispLEGuide, ispTimeZone=ispTimeZone)
 
 if __name__ == '__main__':
     app.run()
